a2d9e6964c4fae98adbe9c344a26fd75397e3bcb
jhay-smart <omalejoy83@gmail.com>
2024-01-20 13:07:39 +0100

    deploying

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,67 +1,65 @@
-// 
-
 let blogId = decodeURI(location.pathname.split("/").pop());
 
 let docRef = db.collection("blogs").doc(blogId);
 
 docRef.get().then((doc) => {
-    if (doc.exists) {
+    if(doc.exists){
         setupBlog(doc.data());
-    } else {
+    } else{
         location.replace("/");
     }
-});
+})
 
 const setupBlog = (data) => {
     const banner = document.querySelector('.banner');
     const blogTitle = document.querySelector('.title');
     const titleTag = document.querySelector('title');
     const publish = document.querySelector('.published');
-
-    // Log the banner image URL
-    console.log("Banner Image URL:", data.bannerImage);
-
-    // Set background image for .banner
-    const ele = document.getElementsByClassName('.bannerimg')
-    banner.style.backgroundImage = `url(${data.bannerImage})`;
     
-
+    banner.style.backgroundImage = `url(${data.bannerImage})`;
 
     titleTag.innerHTML += blogTitle.innerHTML = data.title;
     publish.innerHTML += data.publishedAt;
-    publish.innerHTML += `-- ${data.author}`;
 
     const article = document.querySelector('.article');
     addArticle(article, data.article);
-};
+}
 
 const addArticle = (ele, data) => {
     data = data.split("\n").filter(item => item.length);
+    // console.log(data);
 
     data.forEach(item => {
-        if (item[0] === '#') {
+        // check for heading
+        if(item[0] == '#'){
             let hCount = 0;
             let i = 0;
-            while (item[i] === '#') {
+            while(item[i] == '#'){
                 hCount++;
                 i++;
             }
             let tag = `h${hCount}`;
-            ele.innerHTML += `<${tag}>${item.slice(hCount, item.length)}</${tag}>`;
-        } else if (item[0] === "!" && item[1] === "[") {
-            let separator;
+            ele.innerHTML += `<${tag}>${item.slice(hCount, item.length)}</${tag}>`
+        } 
+        //checking for image format
+        else if(item[0] == "!" && item[1] == "["){
+            let seperator;
 
-            for (let i = 0; i <= item.length; i++) {
-                if (item[i] === "]" && item[i + 1] === "(" && item[item.length - 1] === ")") {
-                    separator = i;
+            for(let i = 0; i <= item.length; i++){
+                if(item[i] == "]" && item[i + 1] == "(" && item[item.length - 1] == ")"){
+                    seperator = i;
                 }
             }
 
-            let alt = item.slice(2, separator);
-            let src = item.slice(separator + 2, item.length - 1);
-            ele.innerHTML += `<img src="${src}" alt="${alt}" class="article-image" onerror="console.error('Error loading image')"/>`;
-        } else {
+            let alt = item.slice(2, seperator);
+            let src = item.slice(seperator + 2, item.length - 1);
+            ele.innerHTML += `
+            <img src="${src}" alt="${alt}" class="article-image">
+            `;
+        }
+
+        else{
             ele.innerHTML += `<p>${item}</p>`;
         }
-    });
-};
+    })
+}
--- public/js/home.js
+++ public/js/home.js
@@ -10,19 +10,12 @@ db.collection("blogs").get().then((blogs) => {
 
 const createBlog = (blog) => {
     let data = blog.data();
-
-    // Check if the bannerImage starts with a relative path
-    let secureBannerImageURL = data.bannerImage.startsWith('/uploads/')
-        ? `${location.origin}${data.bannerImage}`
-        : data.bannerImage;
-
     blogSection.innerHTML += `
     <div class="blog-card">
-        <img src="${secureBannerImageURL}" class="blog-image" alt="">
+        <img src="${data.bannerImage}" class="blog-image" alt="">
         <h1 class="blog-title">${data.title.substring(0, 100) + '...'}</h1>
         <p class="blog-overview">${data.article.substring(0, 200) + '...'}</p>
         <a href="/${blog.id}" class="btn dark">read</a>
     </div>
     `;
 }
-
